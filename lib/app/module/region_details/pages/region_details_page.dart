1d5793b01c109ad9c918703954353026007f9c6f
LuanSouzaProg <luandesouza2001@rede.ulbra.br>
2024-02-12 14:28:44 -0300

    Feature-Ver Região

--- lib/app/module/region_details/pages/region_details_page.dart
+++ lib/app/module/region_details/pages/region_details_page.dart
@@ -1,11 +1,12 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:pay_smart_challenge/app/shared/utils/constraints.dart';
 
-import '../../../shared/utils/flag_mapper.dart';
-import '../bloc/region_details_bloc.dart';
+import '../../../shared/components/animated_component.dart';
+import '../components/details_item_region_component.dart';
 import '../components/details_item_component.dart';
+import '../../../shared/utils/constraints.dart';
+import '../bloc/region_details_bloc.dart';
 
 class RegionDetailsPage extends StatefulWidget {
   final String state;
@@ -43,97 +44,89 @@ class _RegionDetailsPageState extends State<RegionDetailsPage> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Image.asset(
-                      'assets/imgs/doctor_details.png',
-                      height: 200,
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.primaryContainer,
-                        borderRadius: BorderRadius.circular(12),
+                    AnimatedComponent(
+                      delay: 0,
+                      child: Image.asset(
+                        'assets/imgs/doctor_details.png',
+                        height: 200,
                       ),
+                    ),
+                    AnimatedComponent(
+                      delay: 0.5,
+                      child: DetailsItemRegionComponent(
+                        state: region.state ?? '',
+                        name: region.name ?? '',
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    AnimatedComponent(
+                      delay: 1,
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          CircleAvatar(
-                            backgroundImage: AssetImage(
-                                'assets/imgs/flags/Flag_of_${FlagHelper.getFlag(
-                              region.state ?? '',
-                            )}'),
+                          DetailsItemComponent(
+                            title: formatNumber(region.positive ?? 0),
+                            subtitle: 'Positivos',
                           ),
-                          const SizedBox(width: 10),
-                          Text(
-                            region.name ?? '',
-                            style: const TextStyle(
-                              fontSize: 18,
-                            ),
-                          ),
-                          Text(
-                            ' - ${region.state ?? ''} ',
-                            style: const TextStyle(
-                              fontSize: 18,
-                            ),
+                          DetailsItemComponent(
+                            title: formatNumber(region.totalTestResults ?? 0),
+                            subtitle: 'Todas las pruebas',
                           ),
                         ],
                       ),
                     ),
                     const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        DetailsItemComponent(
-                          title: formatNumber(region.positive ?? 0),
-                          subtitle: 'Positivos',
-                        ),
-                        DetailsItemComponent(
-                          title: formatNumber(region.totalTestResults ?? 0),
-                          subtitle: 'Todas las pruebas',
-                        ),
-                      ],
+                    AnimatedComponent(
+                      delay: 1.5,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          DetailsItemComponent(
+                            title: formatNumber(region.death ?? 0),
+                            subtitle: 'Muerto',
+                          ),
+                          DetailsItemComponent(
+                            title: formatNumber(
+                                region.hospitalizedCumulative ?? 0),
+                            subtitle: 'hospitalizados',
+                          ),
+                        ],
+                      ),
                     ),
                     const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        DetailsItemComponent(
-                          title: formatNumber(region.death ?? 0),
-                          subtitle: 'Muerto',
-                        ),
-                        DetailsItemComponent(
-                          title:
-                              formatNumber(region.hospitalizedCumulative ?? 0),
-                          subtitle: 'hospitalizados',
-                        ),
-                      ],
+                    AnimatedComponent(
+                      delay: 2,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          DetailsItemComponent(
+                            title:
+                                formatNumber(region.hospitalizedCurrently ?? 0),
+                            subtitle: 'actualmente hospitalizado',
+                          ),
+                          DetailsItemComponent(
+                            title: formatarData(region.lastUpdateEt.toString()),
+                            subtitle: 'última actualización',
+                          ),
+                        ],
+                      ),
                     ),
                     const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        DetailsItemComponent(
-                          title:
-                              formatNumber(region.hospitalizedCurrently ?? 0),
-                          subtitle: 'actualmente hospitalizado',
-                        ),
-                        DetailsItemComponent(
-                          title: formatarData(region.lastUpdateEt.toString()),
-                          subtitle: 'última actualización',
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        DetailsItemComponent(
-                          title: formatNumber(region.positiveTestsViral ?? 0),
-                          subtitle: 'Pruebas positivas',
-                        ),
-                        DetailsItemComponent(
-                          title: formatNumber(region.negativeTestsViral ?? 0),
-                          subtitle: 'Pruebas negativas',
-                        ),
-                      ],
+                    AnimatedComponent(
+                      delay: 2.5,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          DetailsItemComponent(
+                            title: formatNumber(region.positiveTestsViral ?? 0),
+                            subtitle: 'Pruebas positivas',
+                          ),
+                          DetailsItemComponent(
+                            title: formatNumber(region.negativeTestsViral ?? 0),
+                            subtitle: 'Pruebas negativas',
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
